2b70b26e29faf317a09665ce8a53286404eadf35
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-08-27 16:04:43 -0700

    feat: basics with active navlink

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
-	"net/http"
-
 	"gomaketest/views/pages"
+	"net/http"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	component := pages.HomePage()
+	component := pages.HomePage(r.URL.Path)
+	component.Render(r.Context(), w)
+}
+
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	component := pages.AboutPage(r.URL.Path)
 	component.Render(r.Context(), w)
 }
