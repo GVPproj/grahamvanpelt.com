88306fac64250da8073b4daea6502af78b1d9234
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-08-27 16:21:40 -0700

    fix: track main

---
+++ cmd/gomaketest/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"gomaketest/internal/handler"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "serve" {
+		startServer()
+	} else {
+		fmt.Println("Usage: gomaketest serve")
+	}
+}
+
+func startServer() {
+	mux := http.NewServeMux()
+
+	// Static files
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Routes
+	mux.HandleFunc("/", handler.HomePage)
+	mux.HandleFunc("/about", handler.AboutPage)
+
+	port := ":8080"
+	fmt.Printf("Server starting on http://localhost%s\n", port)
+
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatal(err)
+	}
+}
