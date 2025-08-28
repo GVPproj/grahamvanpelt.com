73fc1668b0be7d02b88416aee0849f606e1e37c0
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-08-27 17:00:05 -0700

    chore: port var

--- cmd/music/main.go
+++ cmd/music/main.go
@@ -12,7 +12,8 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "serve" {
 		startServer()
 	} else {
-		fmt.Println("Usage: music serve")
+		// Default to serving if no args (for Railway)
+		startServer()
 	}
 }
 
@@ -26,8 +27,12 @@ func startServer() {
 	mux.HandleFunc("/", handler.HomePage)
 	mux.HandleFunc("/about", handler.AboutPage)
 
-	port := ":8080"
-	fmt.Printf("Server starting on http://localhost%s\n", port)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	port = ":" + port
+	fmt.Printf("Server starting on port %s\n", port)
 
 	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatal(err)
